266a6ed87e8654c01b053f2c5750b0b61005b788
lifeng617 <lifeng@My-Mac.local>
2014-02-04 12:00:07 +0800

    second test for library

---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, '7.0'
+
+xcodeproj 'SRBeaconSDK/SRBeaconSDK'
+
+pod 'EstimoteSDK', '1.2.0'
---
+++ SRBeaconSDK/SRBeaconSDKTests/SRBeaconSDKTests.m
@@ -0,0 +1,34 @@
+//
+//  SRBeaconSDKTests.m
+//  SRBeaconSDKTests
+//
+//  Created by lifeng on 2/4/14.
+//  Copyright (c) 2014 Kevin McCurdy. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SRBeaconSDKTests : XCTestCase
+
+@end
+
+@implementation SRBeaconSDKTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
